f2e1687a6966fe6d9f09afee4bc14c69cb75a989
byunfi <byunfi@icloud.com>
2019-10-29 10:17:02 +0800

    Initial Commit

---
+++ FateGO Wiki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FateGO Wiki
+//
+//  Created by 白云飞 on 2019/10/29.
+//  Copyright © 2019 白云飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
